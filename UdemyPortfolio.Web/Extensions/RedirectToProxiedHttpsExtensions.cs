b0fe185140c3a22c43d5a555175fb34ae7d060ea
Lautaro Carro <lautaroecarro@gmail.com>
2020-01-03 17:24:14 -0300

    Clean Up

--- UdemyPortfolio.Web/Extensions/RedirectToProxiedHttpsExtensions.cs
+++ UdemyPortfolio.Web/Extensions/RedirectToProxiedHttpsExtensions.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Rewrite;
+﻿using Microsoft.AspNetCore.Rewrite;
+
 using UdemyPortfolio.Web.Rules;
 
 namespace UdemyPortfolio.Web.Extensions
